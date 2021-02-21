ccad1699a934be52b02ef643d253323341ff0b22
Guilherme Jordão <38707597+jordaomoraesl@users.noreply.github.com>
2021-02-21 10:45:53 -0300

    inicio

--- primeiro-react/src/pages/Dashboard/index.tsx
+++ primeiro-react/src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => 
+<h1>Explore Repositorios no GitHub</h1>;
 
 export default Dashboard;
